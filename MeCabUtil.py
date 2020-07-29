0ca8408fc1ea29b265bf06f455a51e06bfb0ea0a
ezreal12 <ezreal12@naver.com>
2020-07-30 01:58:20 +0900

    트레이싱 서버 : 한글자짜리 명사는 기록하지 않기

--- MeCabUtil.py
+++ MeCabUtil.py
@@ -17,7 +17,9 @@ def parseToArrStr(text):
 # text를 입력받고 형태소 분석한 뒤 문자 배열로 리턴하기
 # 형태소중에 "명사"만 리턴함 (NNP,NNG)
 # 청량리역: NNP 진심: NNG
-def parseToArrStrNN(text):
+# textLimit = 해당 변수값 이하의 길이를 가진 word는 입력받지 않음.
+# 즉, textLimit가 1이면 한글자짜리 word는 기록하지않음.
+def parseToArrStrNN(text,textLimit=1):
     node = mecabTagger.parseToNode(text)
     result = []
     while node:
@@ -25,7 +27,8 @@ def parseToArrStrNN(text):
         hinshi = node.feature.split(",")[0]
         if (hinshi != "BOS/EOS"):
             if(hinshi == "NNP" or hinshi == "NNG"):
-                result.append(word)
+                if(len(word)>textLimit):
+                    result.append(word)
         # print(word+": "+hinshi)
         node = node.next
     return result
