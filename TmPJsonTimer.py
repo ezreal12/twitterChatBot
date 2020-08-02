ad4b7887152a3b6fb14e0b6dedfd2eed37c7c9b9
ezreal12 <ezreal12@naver.com>
2020-08-02 20:45:20 +0900

    트레이싱 서버: 서버 최초실행시 바로 tmp 갱신해버리는 문제 수정

--- TmPJsonTimer.py
+++ TmPJsonTimer.py
@@ -7,14 +7,23 @@ import TraceDataManager
 # 4. 가져온 5개의 word를 tmp가 아닌 진짜 screen_name.json에 저장한다.
 class Timer:
     def __init__(self,timeSec):
+        # Tmp 전환은 스케쥴러의 두번째부터 실행가능하게끔
+        # 즉 서버 켜지자마자 tmp 갱신하는짓은 안하게끔
+        # isFirst는 최초실행시까지 True였다가 
+        self.isFirst = True
         self.timeSec = timeSec
         self.Task()
 
     def Task(self):
         # 일정 시간마다 실행하고 싶은 내용을 아래 넣을것
         # 일정 시간마다 tmp 파일 체크해서 word의 json 파일로 바꿔주기
+        # 두번째 실행부터 tmp 갱신
         print("-- checkTmpFiles Timer!!")
-        TraceDataManager.checkTmpFiles()
+        if(self.isFirst==True):
+            self.isFirst=False
+        else:
+            TraceDataManager.checkTmpFiles()
+            
         threading.Timer(self.timeSec, self.Task).start()
         
         
